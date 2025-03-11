590c327bf85a764e8666c813468ea240cb2963db
Rayen123dev <benmassoudrayen7@gmail.com>
2025-03-11 01:06:11 +0100

    aaaaa

---
+++ Back-Office/src/main/java/tn/esprit/projet_pi/entity/ForgotPasswordRequest.java
@@ -0,0 +1,14 @@
+package tn.esprit.projet_pi.entity;
+
+
+public class ForgotPasswordRequest {
+    public String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
